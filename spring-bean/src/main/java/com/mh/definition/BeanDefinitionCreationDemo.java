0077b34fdcf52144d6530b3ed5ce190c7a4588a1
mh <shanqiu.mh@raycloud.com>
2023-02-05 17:11:04 +0800

    add Beandefinition

---
+++ spring-bean/src/main/java/com/mh/definition/BeanDefinitionCreationDemo.java
@@ -0,0 +1,35 @@
+package com.mh.definition;
+
+import com.mh.model.User;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+
+public class BeanDefinitionCreationDemo {
+    public static void main(String[] args) {
+        //1.通过BeanDefinition构建
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        //通过属性设置
+        beanDefinitionBuilder.addPropertyValue("name", "test")
+                .addPropertyValue("age", 34);
+        //获取实例
+        AbstractBeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+        //beanDefinition 并非 Bean 终态，可以自定义修改
+
+        //2 通过AbstractBeanDefinition 以及派生类
+        GenericBeanDefinition genericBeanDefinition = new GenericBeanDefinition();
+        //设置bean类型
+        genericBeanDefinition.setBeanClass(User.class);
+        // 通过MutablePropertyValues 设置
+        MutablePropertyValues propertyValues = new MutablePropertyValues();
+//        propertyValues.addPropertyValue("id", 1);
+//        propertyValues.addPropertyValue("name", "test");
+        propertyValues.add("id", "1")
+                .add("name", "test");
+
+        genericBeanDefinition.setPropertyValues(propertyValues);
+
+
+    }
+}
